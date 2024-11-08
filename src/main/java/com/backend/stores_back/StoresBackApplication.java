65c787a2bb4623c68efeca2a9a546b4fc36614f2
spsiliras <104709495+spsiliras@users.noreply.github.com>
2024-11-08 10:25:29 +0200

    Initial commit

---
+++ src/main/java/com/backend/stores_back/StoresBackApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stores_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoresBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoresBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/stores_back/StoresBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stores_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoresBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
