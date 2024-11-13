ac3bcaf8660e8308a3e9d684c0ed006c361e9820
spsiliras <104709495+spsiliras@users.noreply.github.com>
2024-11-13 16:24:36 +0200

    finished employee and product repos

--- src/main/java/com/backend/stores_back/controller/EmployeeController.java
+++ src/main/java/com/backend/stores_back/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
+@RequestMapping("/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -20,14 +21,14 @@ public class EmployeeController {
     @Autowired
     private StoreRepository storeRepository;
 
-    @GetMapping("/employees/{store_id}")
+    @GetMapping("/{store_id}")
     public List<Employee> getEmployees(@PathVariable int store_id) {
         Optional<Store> store = storeRepository.findById(store_id);
 
         return store.get().getEmployees();
     }
 
-    @PostMapping("/employees/add/{store_id}")
+    @PostMapping("/add/{store_id}")
     public Employee addEmployee(@RequestBody Employee newEmployee, @PathVariable int store_id) {
         Employee employee = storeRepository.findById(store_id).map(store -> {
             newEmployee.setStore(store);
@@ -35,5 +36,28 @@ public class EmployeeController {
         }).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
 
         return employee;
+    }
+
+    @PostMapping("/edit/{employee_id}")
+    public Employee editEmployee(@RequestBody Employee newEmployee, @PathVariable int employee_id) {
+        Employee employee = employeeRepository.findById(employee_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with " + employee_id + " not found"));
+
+        employee.setName(newEmployee.getName());
+        employee.setAddress(newEmployee.getAddress());
+        employee.setSalary(newEmployee.getSalary());
+
+        return employeeRepository.save(employee);
+    }
+
+    @DeleteMapping("/delete/{store_id}/{employee_id}")
+    public void deleteEmployee(@PathVariable int employee_id, @PathVariable int store_id) {
+        Optional<Employee> employee = employeeRepository.findById(employee_id);
+
+        Optional<Store> store = storeRepository.findById(store_id);
+
+        store.get().getEmployees().remove(employee.get());
+
+        employeeRepository.deleteById(employee_id);
     }
 }
---
+++ src/main/java/com/backend/stores_back/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.backend.stores_back.controller;
+
+import com.backend.stores_back.exception.ResourceNotFoundException;
+import com.backend.stores_back.model.Product;
+import com.backend.stores_back.model.Store;
+import com.backend.stores_back.repository.ProductRepository;
+import com.backend.stores_back.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @GetMapping("/{store_id}")
+    public List<Product> getProducts(@PathVariable int store_id) {
+        Optional<Store> store = storeRepository.findById(store_id);
+
+        return store.get().getProducts();
+    }
+
+    @PostMapping("/add/{store_id}")
+    public Product addProduct(@RequestBody Product newProduct, @PathVariable int store_id) {
+        Product product = storeRepository.findById(store_id).map(store -> {
+            newProduct.setStore(store);
+            return productRepository.save(newProduct);
+        }).orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+        return product;
+    }
+
+    @PostMapping("/edit/{product_id}")
+    public Product editProduct(@RequestBody Product newProduct, @PathVariable int product_id) {
+        Product product = productRepository.findById(product_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with " + product_id + " not found"));
+
+        product.setName(newProduct.getName());
+        product.setQuantity(newProduct.getQuantity());
+        product.setPrice(newProduct.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping("/delete/{store_id}/{product_id}")
+    public void deleteProduct(@PathVariable int product_id, @PathVariable int store_id) {
+        Optional<Product> product = productRepository.findById(product_id);
+
+        Optional<Store> store = storeRepository.findById(store_id);
+
+        store.get().getProducts().remove(product.get());
+
+        productRepository.deleteById(product_id);
+    }
+}
